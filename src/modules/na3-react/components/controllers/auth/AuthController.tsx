ea010d7dd75b19bd2baf3cf9d4aaea6c3acd7023
santosmarco <eu@marco.rio.br>
2021-12-02 20:18:23 -0300

    Cleaned up and prettified AuthController

--- src/modules/na3-react/components/controllers/auth/AuthController.tsx
+++ src/modules/na3-react/components/controllers/auth/AuthController.tsx
@@ -1,13 +1,11 @@
 import type { FirebaseError } from "@modules/firebase-errors-pt-br";
 import type { Na3User } from "@modules/na3-types";
 import firebase from "firebase";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import useLocalStorage from "react-use-localstorage";
 
-import { useNa3Users, useStateSlice } from "../../../hooks";
+import { useStateSlice } from "../../../hooks";
 import {
   setAuthError,
   setAuthFirebaseUser,
@@ -18,104 +16,98 @@ import { resolveCollectionId, timestamp } from "../../../utils";
 
 export function Na3AuthController(): null {
   const { environment, messagingTokensStorageKey } = useStateSlice("config");
-  const { _firebaseUser, user, loading: authLoading } = useStateSlice("auth");
+  const { user } = useStateSlice("auth");
 
   const dispatch = useDispatch();
+
   const [storedMessagingTokens, setStoredMessagingTokens] = useLocalStorage(
     messagingTokensStorageKey,
     "[]"
   );
 
-  const [didUpdateLastSeen, setDidUpdateLastSeen] = useState(false);
-
-  const {
-    helpers: { extractRegistrationIdFromEmail },
-  } = useNa3Users();
-
-  const [fbUser, fbUserLoading, fbUserError] = useAuthState(firebase.auth());
-  const [na3UserCandidates, na3UserCandidatesLoading, na3UserCandidatesError] =
-    useCollectionData<Omit<Na3User, "uid">, "uid", "ref">(
-      firebase
-        .firestore()
-        .collection(resolveCollectionId("users", environment))
-        .where(
-          "registrationId",
-          "==",
-          extractRegistrationIdFromEmail(fbUser?.email) || null
-        ),
-      { idField: "uid", refField: "ref" }
-    );
-
-  /* Auth state management hooks */
+  const usersCollectionRef = useRef(
+    firebase.firestore().collection(resolveCollectionId("users", environment))
+  );
 
   useEffect(() => {
-    dispatch(setAuthFirebaseUser(fbUser));
-  }, [dispatch, fbUser]);
+    let unsubscribeFromUserChanges: (() => void) | undefined = undefined;
 
-  useEffect(() => {
-    dispatch(setAuthLoading(fbUserLoading || na3UserCandidatesLoading));
-  }, [dispatch, fbUserLoading, na3UserCandidatesLoading]);
-
-  useEffect(() => {
-    dispatch(setAuthError(fbUserError || na3UserCandidatesError || null));
-  }, [dispatch, fbUserError, na3UserCandidatesError]);
-
-  useEffect(() => {
-    dispatch(setAuthUser(na3UserCandidates?.[0] || null));
-  }, [dispatch, na3UserCandidates]);
-
-  useEffect(() => {
-    void (async (): Promise<void> => {
-      if (_firebaseUser && !user && !authLoading) {
-        dispatch(setAuthLoading(true));
-        dispatch(setAuthError(null));
-
-        try {
-          const userSnapshot = await firebase
-            .firestore()
-            .collection(resolveCollectionId("users", environment))
-            .doc(_firebaseUser.uid)
-            .get();
-          const user = {
-            ...(userSnapshot.data() as Omit<Na3User, "uid">),
-            uid: userSnapshot.id,
-          };
-
-          dispatch(setAuthUser(user));
-        } catch (err) {
-          dispatch(setAuthError(err as FirebaseError));
-        } finally {
+    const unsubscribeFromAuthStateChanges = firebase
+      .auth()
+      .onAuthStateChanged(async (fbUser) => {
+        if (!fbUser) {
+          // Signed out.
+          dispatch(setAuthFirebaseUser(null));
+          dispatch(setAuthUser(null));
           dispatch(setAuthLoading(false));
+          dispatch(setAuthError(null));
+        } else {
+          // Set auth's _firebaseUser.
+          dispatch(setAuthFirebaseUser(fbUser));
+          // Set auth's loading to true. We will start fetching the user.
+          dispatch(setAuthLoading(true));
+
+          // The user's Firestore reference based on its uid.
+          const userRef = usersCollectionRef.current.doc(fbUser.uid);
+
+          try {
+            const userSnapshot = await userRef.get();
+            const user: Na3User = {
+              ...(userSnapshot.data() as Omit<Na3User, "uid">),
+              uid: userSnapshot.id,
+            };
+
+            // Set auth's user.
+            dispatch(setAuthUser(user));
+
+            // Subscribe to changes in the user's data.
+            unsubscribeFromUserChanges = userRef.onSnapshot(
+              (updatedUserSnapshot) => {
+                const updatedUser: Na3User = {
+                  ...(updatedUserSnapshot.data() as Omit<Na3User, "uid">),
+                  uid: updatedUserSnapshot.id,
+                };
+                // Update auth's user.
+                dispatch(setAuthUser(updatedUser));
+              }
+            );
+
+            // Update user's lastSeenAt field.
+            const updatedUserLastSeenAt: Pick<Na3User, "lastSeenAt"> = {
+              lastSeenAt: timestamp(),
+            };
+            void userRef.update(updatedUserLastSeenAt);
+          } catch (err) {
+            dispatch(setAuthError(err as FirebaseError));
+          } finally {
+            dispatch(setAuthLoading(false));
+          }
         }
-      }
-    })();
-  }, [dispatch, _firebaseUser, user, authLoading, environment]);
+      });
 
-  /* Update user's push notification tokens */
+    return (): void => {
+      unsubscribeFromAuthStateChanges();
+      unsubscribeFromUserChanges?.();
+    };
+  }, [dispatch]);
 
+  // Handle updates to the user's notification tokens.
   useEffect(() => {
-    const parsedTokens = JSON.parse(storedMessagingTokens) as Array<string>;
-    if (na3UserCandidates?.[0] && parsedTokens.length > 0) {
-      void na3UserCandidates[0].ref.update({
+    const parsedTokens = JSON.parse(storedMessagingTokens) as string[];
+    // If there's a user and some tokens
+    if (user && parsedTokens.length > 0) {
+      // The user's Firestore reference based on its uid.
+      const userRef = usersCollectionRef.current.doc(user.uid);
+      // Update the user's tokens.
+      void userRef.update({
         notificationTokens: firebase.firestore.FieldValue.arrayUnion(
           ...parsedTokens
         ),
       });
+      // Clear all locally-stored tokens.
       setStoredMessagingTokens("[]");
     }
-  }, [na3UserCandidates, storedMessagingTokens, setStoredMessagingTokens]);
-
-  /* Update user's lastSeenAt field */
-
-  useEffect(() => {
-    if (na3UserCandidates?.[0] && !didUpdateLastSeen) {
-      const updatedUser: Pick<Na3User, "lastSeenAt"> = {
-        lastSeenAt: timestamp(),
-      };
-      void na3UserCandidates[0].ref.update(updatedUser);
-      setDidUpdateLastSeen(true);
-    }
-  }, [na3UserCandidates, didUpdateLastSeen]);
+  }, [user, storedMessagingTokens, setStoredMessagingTokens]);
 
   return null;
 }
