396722f9d3eee6590113bfcac46a6d38178d5171
santosmarco <eu@marco.rio.br>
2021-12-09 12:25:55 -0300

    Added assetlinks.json for Android

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -60,10 +60,7 @@ function registerValidSW(swUrl: string, config?: RegistryConfig): void {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
-                "New content is available and will be used when all " +
-                  "tabs for this page are closed. See https://cra.link/PWA."
-              );
+              console.log("New content available.");
               // Execute callback.
               if (config?.onUpdate) {
                 config.onUpdate(registration);
