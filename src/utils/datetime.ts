0c3c6d79d204debdfca67530d0f2e9a023a7bf51
santosmarco <eu@marco.rio.br>
2021-12-13 14:03:51 -0300

    Added humanizeDuration utility function
    Also:
    - Outsourced timestampToStr options type

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,14 +1,85 @@
-import dayjs, { Dayjs } from "dayjs";
+import dayjs from "dayjs";
+
+const DAYJS_DURATION_UNITS = [
+  "ms",
+  "millisecond",
+  "milliseconds",
+  "s",
+  "second",
+  "seconds",
+  "m",
+  "minute",
+  "minutes",
+  "h",
+  "hour",
+  "hours",
+  "d",
+  "day",
+  "days",
+  "M",
+  "month",
+  "months",
+  "y",
+  "year",
+  "years",
+] as const;
+
+export type DayjsInput = Parameters<typeof dayjs>[0];
+
+type DayjsDuration = plugin.Duration;
+
+type DayjsDurationUnit = typeof DAYJS_DURATION_UNITS[number];
+
+type TimestampToStrOptions = {
+  includeSeconds?: boolean;
+};
+
+type HumanizeDurationOptions = {
+  noSuffix?: boolean;
+};
 
 export function timestamp(): string {
   return dayjs().format();
 }
 
 export function timestampToStr(
-  timestamp: string | Dayjs,
-  options?: { includeSeconds?: boolean }
+  timestamp: DayjsInput,
+  options?: TimestampToStrOptions
 ): string {
   return dayjs(timestamp).format(
     `DD/MM/YY [às] HH:mm${options?.includeSeconds ? ":ss" : ""}`
+  );
+}
+
+export function humanizeDuration(
+  time: number,
+  unit?: DayjsDurationUnit,
+  options?: HumanizeDurationOptions
+): string;
+export function humanizeDuration(
+  from: DayjsInput,
+  to?: DayjsInput,
+  options?: HumanizeDurationOptions
+): string;
+export function humanizeDuration(
+  timeOrFrom: number | DayjsInput,
+  unitOrTo?: DayjsDurationUnit | DayjsInput,
+  options?: HumanizeDurationOptions
+): string {
+  let duration: DayjsDuration;
+  if (
+    typeof timeOrFrom === "number" &&
+    (unitOrTo === undefined || isDayjsDurationUnit(unitOrTo))
+  ) {
+    duration = dayjs.duration(timeOrFrom, unitOrTo);
+  } else {
+    duration = dayjs.duration(dayjs(timeOrFrom).diff(dayjs(unitOrTo)));
+  }
+  return duration.humanize(!options?.noSuffix);
+}
+
+function isDayjsDurationUnit(test: unknown): test is DayjsDurationUnit {
+  return (
+    typeof test === "string" && !!DAYJS_DURATION_UNITS.find((u) => u === test)
   );
 }
