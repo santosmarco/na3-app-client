2479e98dd3b97b696eaa7919eb9e2b22df996f67
santosmarco <eu@marco.rio.br>
2021-12-15 17:04:23 -0300

    Changed text of ReadPendingAlert
    Now the user doens't scroll - he clicks on the action btn instead

--- src/components/app/docs/standards/details/DocsStdReadPendingAlert.tsx
+++ src/components/app/docs/standards/details/DocsStdReadPendingAlert.tsx
@@ -4,8 +4,9 @@ import React from "react";
 export function DocsStdReadPendingAlert(): JSX.Element {
   return (
     <PageAlert marginBottom="small" title="Leitura pendente" type="info">
-      <strong>Atenção!</strong> Você ainda não marcou esta versão do documento
-      como lida — role até o final da página e comunique sua leitura.
+      <strong>Atenção!</strong> Ainda não identificamos sua leitura deste
+      documento. Por favor, clique em &quot;Acessar documento&quot; e o leia até
+      o final para registrá-la.
     </PageAlert>
   );
 }
