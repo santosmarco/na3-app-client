d11ec66394319b8a4bf480993a9f939c417d2929
santosmarco <eu@marco.rio.br>
2021-12-17 09:58:11 -0300

    Added optional opacity prop to <Logo />

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -9,24 +9,33 @@ type LogoProps = {
   className?: string;
   height?: number;
   theme?: ThemeMode;
+  opacity?: number;
 };
 
 export function Logo({
   height,
   className,
   theme: themeProp,
+  opacity,
 }: LogoProps): JSX.Element {
   const [appTheme] = useTheme();
 
-  const theme = useMemo(() => themeProp || appTheme, [themeProp, appTheme]);
+  const theme = useMemo(() => {
+    if (themeProp) {
+      return themeProp;
+    }
+    return appTheme === "dark" ? "light" : "dark";
+  }, [themeProp, appTheme]);
+
+  const containerStyle = useMemo(() => ({ opacity: opacity ?? 1 }), [opacity]);
 
   return (
-    <div className={className}>
+    <div className={className} style={containerStyle}>
       <img
         alt="Logotipo Nova A3"
         className={ANIMATION_FADE_IN}
         height={height || 32}
-        src={theme === "light" ? logoDark : logoLight}
+        src={theme === "light" ? logoLight : logoDark}
         width={height ? height * (140 / 32) : 140}
       />
     </div>
