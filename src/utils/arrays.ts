13629fcb29d2d5df226f585437f8d2fe40aa2a38
santosmarco <eu@marco.rio.br>
2021-12-22 17:24:02 -0300

    Added `compareArrays` helper fn

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -15,3 +15,10 @@ export function handleFilterDuplicates<T extends Primitive>(
 export function handleFilterFalsies<T>(el: T): el is Exclude<T, Falsy> {
   return !!el;
 }
+
+export function compareArrays<T, U>(a: T[], b: U[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+  return JSON.stringify(a) === JSON.stringify(b);
+}
